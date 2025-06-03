b30e52b1c5a1171c635d1b3670c6108018874b8c
FelipeKogake <felipe.kogake77@gmail.com>
2025-06-03 19:13:38 -0300

    ajustado algumas melhorias mas não tudo, implementado a reta numérica dinamica e mudando o objeto

--- mru.js
+++ mru.js
@@ -1,7 +1,11 @@
+var pista;
+var carroImg;
 const objeto = document.getElementById("retangulo")
 const escala = 23.25*0.01;
+const caractere = "|";
 
 let intervalo;
+
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -15,8 +19,45 @@ async function moverObjeto() {
     const velocidade = Number(inputvelocidade.value)
     const inputTempoTotal = document.getElementById("tempoTotal")
     const tempoTotal = Number(inputTempoTotal.value)
-    
+    const caminho = document.getElementById("caminho")
+    const linha = document.getElementById("lv")
 
+    // const repeticoes = posicaoInicial + velocidade * tempoTotal;
+    // const caractere = "|";
+    // const container = document.getElementById("retaNumerica");
+
+    // // Limpa o conteúdo anterior (se necessário)
+    // container.innerHTML = "";
+
+    // for (let i = 0; i < repeticoes; i++) {
+    //     const p = document.createElement("p");
+    //     p.textContent = caractere;
+    //     p.classList.add("traco"); // Estilo individual
+    //     container.appendChild(p);
+    // }
+
+    const repeticoes = posicaoInicial + velocidade * tempoTotal + 10;
+    const caractere = "|";
+    const container = document.getElementById("retaNumerica");
+
+    container.innerHTML = "";
+
+    for (let i = 0; i < repeticoes; i++) {
+    const p = document.createElement("p");
+
+        if (i % 10 === 0) {
+            const numeroFormatado = i.toString().padStart(3, "0");
+            p.textContent = numeroFormatado;
+            p.classList.add("numero");
+        } else {
+            p.textContent = "|";
+            p.classList.add("traco");
+        }
+
+        container.appendChild(p);
+    }
+    
+    linha.style.width = "100%"
     objeto.style.left = (posicaoInicial * 23) + "px"
 
     await sleep(2000);
@@ -27,11 +68,12 @@ async function moverObjeto() {
 
     intervalo = setInterval(() => {
         let tempoPassado = (Date.now() - inicioTempo) / 1000
+        caminho.scrollLeft = passo -500
 
         if(tempoPassado < tempoTotal) {
-            passo += escala * velocidade
+            passo += escala * velocidade 
 
-            objeto.style.left = passo + "px"
+            objeto.style.left = passo+10 + "px"
         }
         else{
             clearInterval(intervalo)
